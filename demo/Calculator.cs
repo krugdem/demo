6c3409e6331130c8384f9416f4fb0af6c128e2c1
krugdemo <gariev.do@gmail.com>
2026-03-06 13:09:45 +0500

    Add class Calculator

---
+++ demo/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace demo
+{
+    public class Calculator
+    {
+        public int add(int x, int y)
+        {
+            if (x < 1 || y < 1)
+            {
+                throw new ArgumentException("Метод принимает только целые положительные числа");
+            }
+
+            return checked(x + y);
+        }
+    }
+}
